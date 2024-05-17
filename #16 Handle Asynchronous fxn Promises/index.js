f9e9fa803b29e517971d6a6e74f6e3cfaac62398
rahulsharma376 <rahul.tsharma.376@gmail.com>
2024-05-17 17:48:45 +0530

    html json render

---
+++ #16 Handle Asynchronous fxn Promises/index.js
@@ -0,0 +1,17 @@
+let a = 20;
+let b = 0;
+
+console.log("start");
+
+let waitingData = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(30)
+    }, 2000)
+})
+
+waitingData.then((data) => {
+    b = data;
+    console.log(a+b);
+})
+
+console.log("end");
