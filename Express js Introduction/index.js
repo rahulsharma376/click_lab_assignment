38743d61a2d565a973a502a0f4ae7a1baf55aede
rahulsharma376 <rahul.tsharma.376@gmail.com>
2024-05-18 02:52:49 +0530

    make html page

--- Express js Introduction/index.js
+++ Express js Introduction/index.js
@@ -1,42 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const publicPath = path.join(__dirname,'public');
 
-app.get('', (req, res) =>{
-    res.send(`
-    <h1>Hello World. Nice to meet you. Its a homepage.</h1>
-    <a href="/about">Go to About Section </a>
-    `);
-});
-
-
-app.get('/about', (req, res) =>{
-    res.send(`
-    <input type="text" placeholder="user name" value="${req.query.name}" />
-    <button><a href="/">submit here</a></button>
-    <a href = "/">Go to Home Page</a>
-    `);
-});
-
-app.get('/help', (req, res) =>{
-    res.send(
-        [
-            {
-                name: 'Ankit',
-                address: '38 Para Resue street',
-                email: 'ankit@tester.com'
-            },
-            {
-                name: 'Arvind Kejriwal',
-                address: 'Delhi',
-                email: 'Delhi@pradhanmantri.com'
-            },
-            {
-                name: 'youte Bent',
-                address: 'vone Q1 street',
-                email: 'bent@handler.com'
-            }
-        ]
-    );
-});
+app.use(express.static(publicPath));
 
 app.listen(5000);
