d6875525a950dcf5d198482c2b2bd59a2ffcfe31
rahulsharma376 <rahul.tsharma.376@gmail.com>
2024-05-17 17:41:52 +0530

    Html json render #20

--- Express js Introduction/index.js
+++ Express js Introduction/index.js
@@ -2,15 +2,41 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) =>{
-    res.send('Hello World. Nice to meet you.');
+    res.send(`
+    <h1>Hello World. Nice to meet you. Its a homepage.</h1>
+    <a href="/about">Go to About Section </a>
+    `);
 });
 
-app.get('/home', (req, res) =>{
-    res.send("Hello, It's home page");
-});
 
 app.get('/about', (req, res) =>{
-    res.send("Hello, It's About section.");
+    res.send(`
+    <input type="text" placeholder="user name" value="${req.query.name}" />
+    <button><a href="/">submit here</a></button>
+    <a href = "/">Go to Home Page</a>
+    `);
+});
+
+app.get('/help', (req, res) =>{
+    res.send(
+        [
+            {
+                name: 'Ankit',
+                address: '38 Para Resue street',
+                email: 'ankit@tester.com'
+            },
+            {
+                name: 'Arvind Kejriwal',
+                address: 'Delhi',
+                email: 'Delhi@pradhanmantri.com'
+            },
+            {
+                name: 'youte Bent',
+                address: 'vone Q1 street',
+                email: 'bent@handler.com'
+            }
+        ]
+    );
 });
 
 app.listen(5000);
