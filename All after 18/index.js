564c7b01300b31494a999fd3884fcd5e700b58a3
rahulsharma376 <rahul.tsharma.376@gmail.com>
2024-05-22 15:53:36 +0530

    html extension remove #22 and ejs template engine #23

--- All after 18/index.js
+++ All after 18/index.js
@@ -4,6 +4,35 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname,'public');
 
-app.use(express.static(publicPath));
+app.set('view engine','ejs');
 
+app.get('/profile', (_,res) => {
+    const user = {
+        name: 'Rahul',
+        email: 'rahul.376@developer.com',
+        city: 'Banur, Punjab'
+    }
+    res.render(`profile`, {user});
+})
+
+// app.use(express.static(publicPath));
+app.get('/', (_,res) => {
+    res.sendFile(`${publicPath}/index.html`)
+});
+
+app.get('/about', (_,res) => {
+    res.sendFile(`${publicPath}/about.html`)
+});
+
+app.get('/help', (_,res) => {
+    res.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('/help', (_,res) => {
+    res.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('/*', (_,res) => {
+    res.sendFile(`${publicPath}/index.html`)
+});
 app.listen(5000);
